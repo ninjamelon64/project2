de5f8c64a01fe637f939242940c879a257001123
ninjamelon64 <58517242+ninjamelon64@users.noreply.github.com>
2025-02-16 22:08:03 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
@@ -10,6 +8,7 @@ public class Main {
         boolean isZero = false;
         while (!isZero) {
             System.out.println("""
+            
             Please choose an option:
             (1) Add a task.
             (2) Remove a task.
@@ -22,6 +21,7 @@ public class Main {
             switch(request) {
                 case "0":
                     isZero = true;
+                    System.out.println("Goodbye!");
                     break;
                 case "1":
                     addTask(tasks);
@@ -42,7 +42,6 @@ public class Main {
                     System.out.println("Invalid option! Enter a number 0-5");
                     break;
             }
-            System.out.println("Done!");
         }
     }
     static void addTask(ArrayList<Task> tasks) {
@@ -51,22 +50,38 @@ public class Main {
         System.out.println("What is the description for this task?");
         String desc = input.nextLine();
         int prior = -1;
-        while (prior < 0 | prior > 5) {
+        while (prior < 0 | prior > 6) {
             System.out.println("What is the priority of this task? (1-5)");
-            prior = input.nextInt();
+            try {
+                prior = input.nextInt();
+                if (prior < 0 | prior > 6) {
+                    System.out.println("Invalid input for priority!");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input for priority!");
+            }
             input.nextLine();
         }
         tasks.add(new Task(name, desc, prior));
+        System.out.println("Done!");
     }
     static void removeTask(ArrayList<Task> tasks) {
         if (!tasks.isEmpty()) {
             int toRemove = -1;
             while (toRemove < 0 | toRemove > tasks.size() - 1) {
                 System.out.println("What is the index of the task to remove?");
-                toRemove = input.nextInt();
+                try {
+                    toRemove = input.nextInt();
+                    if (toRemove < 0 | toRemove > 6) {
+                        System.out.println("Invalid input for index!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input for index!");
+                }
                 input.nextLine();
             }
             tasks.remove(toRemove);
+            System.out.println("Done!");
         } else {
             System.out.println("You have no tasks to remove!");
         }
@@ -76,7 +91,14 @@ public class Main {
             int index = -1;
             while (index < 0 | index > tasks.size() - 1) {
                 System.out.println("What is the index of the task to change?");
-                index = input.nextInt();
+                try {
+                    index = input.nextInt();
+                    if (index < 0 | index > 6) {
+                        System.out.println("Invalid input for index!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input for index!");
+                }
                 input.nextLine();
             }
             System.out.println("What task would you like to add?");
@@ -84,30 +106,56 @@ public class Main {
             System.out.println("What is the description for this task?");
             String desc = input.nextLine();
             int prior = -1;
-            while (prior < 0 | prior > 5) {
+            while (prior < 0 | prior > 6) {
                 System.out.println("What is the priority of this task? (1-5)");
-                prior = input.nextInt();
+                try {
+                    prior = input.nextInt();
+                    if (prior < 0 | prior > 6) {
+                        System.out.println("Invalid input for priority!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input for priority!");
+                }
                 input.nextLine();
             }
             tasks.set(index, new Task(name, desc, prior));
+            System.out.println("Done!");
         } else {
             System.out.println("You have no tasks to update!");
         }
     }
-
     static void listTasks(ArrayList<Task> tasks) {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println(i + ". " + tasks.get(i));
         }
     }
     static void listPriority(ArrayList<Task> tasks) {
-        System.out.println("What priority do you want to list?");
-        int prior = input.nextInt();
-        input.nextLine();
-        for (Task task : tasks) {
-            if (task.getPriority() == prior) {
-                System.out.println(task);
+        if (!tasks.isEmpty()) {
+            int prior = -1;
+            while (prior < 0 | prior > 6) {
+                System.out.println("What priority do you want to list?");
+                try {
+                    prior = input.nextInt();
+                    if (prior < 0 | prior > 6) {
+                        System.out.println("Invalid input for priority!");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input for priority!");
+                }
+                input.nextLine();
             }
+            boolean somethingListed = false;
+            for (Task task : tasks) {
+                if (task.getPriority() == prior) {
+                    System.out.println(task);
+                    somethingListed = true;
+                }
+            }
+            if (!somethingListed) {
+                System.out.println("You have no tasks of that priority!");
+            }
+        } else {
+            System.out.println("You have no tasks to list!");
         }
     }
     static class Task {
@@ -147,10 +195,9 @@ public class Main {
 
         @Override
         public String toString() {
-            return  "\nTask" +
-                    "\nname: " + name +
+            return  "\nname: " + name +
                     "\ndesc: " + desc +
-                    "\npriority: " + prior;
+                    "\npriority: " + prior + "\n";
         }
     }
 }
